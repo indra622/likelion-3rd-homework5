5f824517e4a9b77ce3c1d7b7db960d399eae45d8
hosung <indra622@gmail.com>
2015-07-29 13:36:29 +0900

    SimpleAdding added

---
+++ Coderbyte/SimpleAdding.rb
@@ -0,0 +1,10 @@
+def SimpleAdding(num)
+	a = 0
+	(1..num).each {|p| a += p}
+	return a
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+puts SimpleAdding(STDIN.gets.to_i)  
